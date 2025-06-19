438b4354c16b7f6c456f3443b4de52effcd7657b
Unnati Arya <104190330+unnatiarya1@users.noreply.github.com>
2025-06-19 22:58:41 +0530

    Update settings.py

--- metweather/settings.py
+++ metweather/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'climate',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
+
